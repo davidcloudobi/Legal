bc7ef339f1af6c7f3ce3973f0f6c8b78ca9fa4ec
Davidcloudobi <davidcloudobi@gmail.com>
2020-03-14 18:49:24 +0100

    database update

---
+++ Legal/Pages/Table.cshtml.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace Legal.Pages
+{
+    public class TableModel : PageModel
+    {
+        public void OnGet()
+        {
+
+        }
+    }
+}
